a572156b469a4bbd577ef2e57c6a63db2511ed8f
Dhanushforyou <Dhanushreddy450@gmail.com>
2025-07-05 19:19:35 +0530

    Create main.jsx in src folder

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MeetingSummarizerApp from "./MeetingSummarizerApp";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MeetingSummarizerApp />
+  </React.StrictMode>
+);
