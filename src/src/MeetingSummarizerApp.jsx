eeceacb38ab7bc3f29135d2c0ada40ca0019d6a3
Dhanushforyou <Dhanushreddy450@gmail.com>
2025-07-05 19:17:25 +0530

    Add MeetingSummarizerApp.jsx to src

---
+++ src/src/MeetingSummarizerApp.jsx
@@ -0,0 +1,10 @@
+function MeetingSummarizerApp() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>AI Meeting Summarizer</h1>
+      <p>Upload your audio and get summaries + action items here.</p>
+    </div>
+  );
+}
+
+export default MeetingSummarizerApp;
