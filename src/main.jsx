28ce54919a928251fdd41e595c2fd298e769d2fe
Dhanushforyou <Dhanushreddy450@gmail.com>
2025-07-05 18:46:13 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MeetingSummarizerApp from "./MeetingSummarizerApp";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MeetingSummarizerApp />
+  </React.StrictMode>
+);
+
